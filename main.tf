eb04f650b4c53f187efbb26883eddabcd50595a2
jagadish7-25 <kadapajag7@gmail.com>
2024-10-03 22:57:58 +0200

    adeed

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "aws_subnet" "private" {
   vpc_id     = aws_vpc.main.id
   cidr_block = var.private_subnet_cidrs[count.index]
   availability_zone = local.az_names[count.index]
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false 
   tags = merge(
       var.common_tags,
       var.private_subnet_tags,
@@ -56,7 +56,7 @@ resource "aws_subnet" "database" {
   vpc_id     = aws_vpc.main.id
   cidr_block = var.database_subnet_cidrs[count.index]
   availability_zone = local.az_names[count.index]
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false
   tags = merge(
       var.common_tags,
       var.database_subnet_tags,
