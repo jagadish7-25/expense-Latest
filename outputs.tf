5ab3bc4ecf068c70cd9f7b589494311ac173f514
jagadish7-25 <kadapajag7@gmail.com>
2024-10-05 13:10:34 +0200

    routes and routue tables added

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,10 @@ output "vpc_id" {
     value = aws_vpc.main.id
   
 }
+output "vpc_default" {
+    value = data.aws_vpc.default
+  
+}
+output "main_route_table_info" {
+ value = data.aws_route_table.main
+ }
--- varibles.tf
+++ varibles.tf
@@ -78,3 +78,10 @@ variable "private_route_table_tags" {
 variable "database_route_table_tags" {
   default = {}
 }
+variable "is_peering_required" {
+  type = boolean
+  default = false 
+}
+variable "vpc_peering_tags" {
+  default = {}
+}
